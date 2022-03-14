a34c16b7f16b5d34d5ead788e2943dc4b93b82fa
warsme <jawa0262@stud.kea.dk>
2022-03-14 15:22:02 +0100

    Adventure

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,16 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to Adventure!");    
+
+    Scanner playerInput = new Scanner(System.in);
+
+    String directions = playerInput.next();
+
+        System.out.println();
+
+
+
 
 
     }
