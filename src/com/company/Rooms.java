b5a82ab10d7584a72a978417a6a6f9669f320328
MartinaFrederiksen <100579531+MartinaFrederiksen@users.noreply.github.com>
2022-03-14 14:50:20 +0100

    adventure

--- src/com/company/Rooms.java
+++ src/com/company/Rooms.java
@@ -1,5 +1,23 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Rooms {
+    Rooms room1 = new Rooms ();
+        //room1.SetEast(room2);
+        //room1.SetSouth(room4);
+    Rooms room2 = new Rooms ();
+    Rooms room3 = new Rooms ();
+    Rooms room4 = new Rooms ();
+    Rooms room5 = new Rooms ();
+    Rooms room6 = new Rooms ();
+    Rooms room7 = new Rooms ();
+    Rooms room8 = new Rooms ();
+    Rooms room9 = new Rooms ();
+
+
+
+
+
 
 }
