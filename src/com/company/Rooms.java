25cd809af85d22d4b9b0f98e8530a5ba06196e1b
MartinaFrederiksen <100579531+MartinaFrederiksen@users.noreply.github.com>
2022-03-14 15:37:59 +0100

    adventure

--- src/com/company/Rooms.java
+++ src/com/company/Rooms.java
@@ -4,6 +4,22 @@ import java.util.Scanner;
 
 public class Rooms {
     Rooms room1 = new Rooms ();
+
+    int directions = 4;
+    switch (directions) {
+        case 1:
+            //code block
+            break;
+        case 2:
+            //code block
+            break;
+        case 3:
+            //code block
+            break;
+        case 4:
+            //code block
+            break;
+    }
         //room1.SetEast(room2);
     //room1.SetSouth(room4);
     Rooms room2 = new Rooms ();
@@ -14,7 +30,6 @@ public class Rooms {
     Rooms room7 = new Rooms ();
     Rooms room8 = new Rooms ();
     Rooms room9 = new Rooms ();
-
 
 
 
